6e4d191d254727dff9a2fafa321e98d9318a71fa
Eric Chee <eric1313@live.com>
2019-07-15 11:32:55 -0700

    fixed no-config flag implementation and cleaned up config parsing

--- controller/controller.go
+++ controller/controller.go
@@ -151,6 +151,7 @@ func (c *Controller) processItem(key string) error {
 	args := []string{"tracker",
 		fmt.Sprintf("--cluster=%s", viper.GetString("cluster")),
 		fmt.Sprintf("--log=%s", viper.GetString("log.level")),
+		fmt.Sprintf("--no-config=true"),
 		fmt.Sprintf("--tracker-kcdapp=%s", kcd.Spec.Selector["kcdapp"]),
 		fmt.Sprintf("--tracker-version=%s", version),
 		fmt.Sprintf("--tracker-namespace=%s", namespace),
